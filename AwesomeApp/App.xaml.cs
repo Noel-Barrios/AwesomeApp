3d591a83c795746d4154747f47a92d591fc2aa68
Noel Barrios <noel.barrios@optum.com>
2019-01-16 14:39:38 -0600

    added comments

--- AwesomeApp/App.xaml.cs
+++ AwesomeApp/App.xaml.cs
@@ -5,12 +5,15 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace AwesomeApp
 {
+    // to maximize reuse of the startup code, Xamarin.Forms applications have a single class named App
+    // that is responsible for instantiating the first Page that will be displayed.
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
+            //sets the App.MainPage property to an instance of the MainPage class
             MainPage = new MainPage();
         }
 
